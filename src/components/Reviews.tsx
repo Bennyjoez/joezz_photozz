0de640a64bc993f0a7347a12f5a6ff0c563cdbc9
Benson <njugunab655@gmail.com>
2024-01-12 10:16:53 +0300

    Duplicate placeholder reviews

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -4,11 +4,27 @@ export default function Reviews() {
   return (
     <section id='reviews-container'>
       <h2 className="heading">Reviews</h2>
-      <div className='review'>
-        <img src={profile} className="review-profile" alt="reviewer" />
-        <div>
-          <h3>Benson Njuguna</h3>
-          <p>"These guys saved the day by covering my event and making it memorable to every detail"</p>
+      <div className="reviews">
+        <div className='review'>
+          <img src={profile} className="review-profile" alt="reviewer" />
+          <div>
+            <h3>Benson Njuguna</h3>
+            <p>"These guys saved the day by covering my event and making it memorable to every detail"</p>
+          </div>
+        </div>
+        <div className='review'>
+          <img src={profile} className="review-profile" alt="reviewer" />
+          <div>
+            <h3>Benson Njuguna</h3>
+            <p>"These guys saved the day by covering my event and making it memorable to every detail"</p>
+          </div>
+        </div>
+        <div className='review'>
+          <img src={profile} className="review-profile" alt="reviewer" />
+          <div>
+            <h3>Benson Njuguna</h3>
+            <p>"These guys saved the day by covering my event and making it memorable to every detail"</p>
+          </div>
         </div>
       </div>
     </section>
