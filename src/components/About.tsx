1b4d8f98f12b2108acc2fd838a9a80323517de28
Benson <njugunab655@gmail.com>
2024-01-29 14:54:57 +0300

    Add about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import shootImage from '../../public/ladyshoot.jpeg';
 
 export default function About() {
   return (
     <section>
-      <h2 className='heading'>About Us</h2>
+      <aside>At Joezz Photography, we believe that every moment tells a unique story. Through the lens, we capture the beauty, emotion, and authenticity of your special occasions, turning them into timeless memories.
+
+      Our team of passionate photographers is dedicated to creating a comfortable and enjoyable experience for our clients. We go beyond capturing images; we strive to narrate your story with creativity and artistry.
+
+      From intimate portraits to grand celebrations, our goal is to provide you with stunning visuals that resonate with the essence of who you are. We understand the importance of preserving life's precious moments, and we consider it an honor to be part of your journey.
+
+      Explore our portfolio, and let the images speak for themselves. We look forward to being your trusted storytellers, turning your moments into cherished memories.
+
+      Thank you for considering [Your Photography Studio Name] for your photographic needs. 🌟
+      </aside>
       <aside>
-        
+        <div>
+          <img src={shootImage} alt="Lady on a shoot" />
+        </div>
       </aside>
     </section>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import Form from "../components/Form";
 import Hero from "../components/Hero";
 import Reviews from "../components/Reviews";
 import Services from "../components/services/Services";
+import About from "../components/About";
 
 export default function Home() {
   return (
@@ -9,6 +10,8 @@ export default function Home() {
       <Hero />
       <h2 className='heading'>Our Services</h2>
       <Services />
+      <h2 className='heading'>About Us</h2>
+      <About />
       <h2 className='heading'>Reviews</h2>
       <Reviews />
       <h2 className='heading'>Book A Session</h2>
