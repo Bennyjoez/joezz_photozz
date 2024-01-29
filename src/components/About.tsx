90cb84c14cb95566ceaf8910d959e5bb7378f763
Benson <njugunab655@gmail.com>
2024-01-29 15:19:09 +0300

    Add the about page description

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import shootImage from '../../public/ladyshoot.jpeg';
 
 export default function About() {
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,10 @@
-export default function About() {
+import About from "../components/About"
+
+export default function AboutPage() {
   return (
-    <div>About</div>
+    <div>
+      <h2 className="heading">About Us</h2>
+      <About />
+    </div>
   )
 }
