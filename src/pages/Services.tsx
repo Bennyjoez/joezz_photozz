862fc91036cb3a03d9ef606038d06c9a562610c5
Benson <njugunab655@gmail.com>
2024-01-29 18:23:38 +0300

    Adjust the sizing of images and containers

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import Services from '../components/services/Services'
 
-export default function Services() {
+export default function ServicesPage() {
   return (
-    <div>Services</div>
+    <div>
+      <h2 className="heading">Our Services</h2>
+      <Services />
+    </div>
   )
 }
