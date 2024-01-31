793973fc7d9ae5d1c1525b12de322aee29232522
Benson <njugunab655@gmail.com>
2024-01-31 17:27:28 +0300

    Add overall toast contaainer

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react';
 import Navbar from './components/Navbar';
+import { ToastContainer } from 'react-toastify';
 
 interface LayoutProps {
   children: ReactNode;
@@ -9,6 +10,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div>
       <Navbar />
+      <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
       <main>{children}</main>
     </div>
   );
