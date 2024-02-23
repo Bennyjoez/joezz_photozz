c2d87cb042ac8e28a1490c49a7498f95bea277da
Benson Njuguna <njugunab655@gmail.com>
2024-02-23 09:09:39 +0300

    Adjust layout configuration

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@
 import React from 'react';
 import Layout from './Layout';
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import Navbar from './components/Navbar';
 
 const App: React.FC = () => {
   return (
     <Layout>
+      <Navbar />
+      <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
       <Outlet />
     </Layout>
   );
