0ebfd92722bd017c397fd5c2075dd4607eac161c
Benson Njuguna <njugunab655@gmail.com>
2024-02-24 19:33:19 +0300

    Save user data on register

--- src/components/authentication/Register.tsx
+++ src/components/authentication/Register.tsx
@@ -1,10 +1,14 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import formImage from '../../../public/form.jpg';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { saveUser } from '../../Features/user/userSlice';
 
 function Register() {
+  const dispatch = useAppDispatch();
+  const loggedInUser = useAppSelector((state) => state.user.name);
   const [user, setUser] = useState({
     name: '',
     email: '',
@@ -41,6 +45,7 @@ function Register() {
       // registration was successful
       const data = await response.json();
       toast.success(data.message);
+      dispatch(saveUser(data.data.user));
     } catch (err: unknown) {
       toast.error(`Registration failed: ${err}`)}
   };
@@ -52,28 +57,34 @@ function Register() {
   };
 
   return (
-    <section className='form-container'>
-      <div className='placeholder-image'>
-        <img src={formImage} alt="form image" />
-      </div>
-      <form onSubmit={handleSubmit} className='signup-form'>
-        <h2 className='heading'>Register</h2>
-        <div className='flex-form'>
-          <label htmlFor="name">Full Name</label>
-          <input type="text" name="name" id="name" value={user.name} onChange={handleInput} required placeholder='Name' />
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" value={user.email} onChange={handleInput} required placeholder='Email' />
-          <label htmlFor="contact">Contact</label>
-          <input type="tel" name="contact" id="contact" value={user.contact} onChange={handleInput} required placeholder='Contact' />
-          <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" value={user.password} onChange={handleInput} minLength={6} required placeholder='Password' />
-          <button type="submit" className='book-session-btn'>Register</button>
-        </div>
-        <p className='register-btn'>
-          You already have an account? <Link to='/login'>Login</Link>
-        </p>
-      </form>
-    </section>
+    <>
+      {
+        loggedInUser ? <Navigate to="/" replace={true} />
+        :
+        <section className='form-container'>
+          <div className='placeholder-image'>
+            <img src={formImage} alt="form image" />
+          </div>
+          <form onSubmit={handleSubmit} className='signup-form'>
+            <h2 className='heading'>Register</h2>
+            <div className='flex-form'>
+              <label htmlFor="name">Full Name</label>
+              <input type="text" name="name" id="name" value={user.name} onChange={handleInput} required placeholder='Name' />
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" id="email" value={user.email} onChange={handleInput} required placeholder='Email' />
+              <label htmlFor="contact">Contact</label>
+              <input type="tel" name="contact" id="contact" value={user.contact} onChange={handleInput} required placeholder='Contact' />
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" id="password" value={user.password} onChange={handleInput} minLength={6} required placeholder='Password' />
+              <button type="submit" className='book-session-btn'>Register</button>
+            </div>
+            <p className='register-btn'>
+              You already have an account? <Link to='/login'>Login</Link>
+            </p>
+          </form>
+        </section>
+      }
+    </>
   )
 }
 
