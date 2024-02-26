2c17222bb879f235e4ed3b8102fa345b55b8a4e8
Benson Njuguna <njugunab655@gmail.com>
2024-02-26 18:39:22 +0300

    Adjust containers in form

--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-export default function Form() {
+export default function BookingForm() {
   const [details, setDetails] = useState({
     name: '',
     email: '',
@@ -22,25 +22,27 @@ export default function Form() {
 
 
   return (
-    <section id='book-session' className="bookings-form-container">
-      <form onSubmit={handleSubmit} className='signup-form'>
-        <div className='flex-form'>
-          <label htmlFor="name">Name:</label>
-          <input type="text" name="name" id="name" placeholder='Your Full Name' onChange={handleInput} />
-          <label htmlFor="email">Email:</label>
-          <input type="email" name="email" id="email" placeholder='Your Email' onChange={handleInput} />
-          <label htmlFor="contact">WhatsApp Contact:</label>
-          <input type="tel" name="contact" id="contact" placeholder='Your WhatsApp Contact' onChange={handleInput} />
-          <label htmlFor="event">Event:</label>
-          <input type="text" name="event" id="event" placeholder='What is the occasion' onChange={handleInput} />
-          <label htmlFor="contact">Shoot Location</label>
-          <input type="text" name="location" id="location" placeholder="Where's the shoot" onChange={handleInput} />
-          <label htmlFor="message">Message:</label>
-          <textarea name="message" id="message" placeholder='What would you like us to know about your booking...' onChange={handleInput}></textarea>
+    <section className="popup-form">
+      <div className='bookings-form-container'>
+        <form onSubmit={handleSubmit} className='signup-form'>
+          <div className='flex-form'>
+            <label htmlFor="name">Name:</label>
+            <input type="text" name="name" id="name" placeholder='Your Full Name' onChange={handleInput} />
+            <label htmlFor="email">Email:</label>
+            <input type="email" name="email" id="email" placeholder='Your Email' onChange={handleInput} />
+            <label htmlFor="contact">WhatsApp Contact:</label>
+            <input type="tel" name="contact" id="contact" placeholder='Your WhatsApp Contact' onChange={handleInput} />
+            <label htmlFor="event">Event:</label>
+            <input type="text" name="event" id="event" placeholder='What is the occasion' onChange={handleInput} />
+            <label htmlFor="contact">Shoot Location</label>
+            <input type="text" name="location" id="location" placeholder="Where's the shoot" onChange={handleInput} />
+            <label htmlFor="message">Message:</label>
+            <textarea name="message" id="message" placeholder='What would you like us to know about your booking...' onChange={handleInput}></textarea>
 
-          <button type="submit" className='book-session-btn'>Submit Booking</button>
-        </div>
-      </form>
+            <button type="submit" className='book-session-btn'>Submit Booking</button>
+          </div>
+        </form>
+      </div>
     </section>
   )
 }
