bcd66542722dc5d4b8ad41398b67138e4ff58442
Benson Njuguna <njugunab655@gmail.com>
2024-02-26 12:24:15 +0300

    Update links

--- src/components/BookBtn.tsx
+++ src/components/BookBtn.tsx
@@ -1,5 +1,22 @@
+import { toast } from "react-toastify";
+import { useAppSelector } from "../app/hooks";
+import { useNavigate } from "react-router-dom";
+
 export default function BookBtn() {
+  const navigate = useNavigate();
+  const userName = useAppSelector((state) => state.user.name);
+
+  const handleClick = () => {
+    // If logged in, navigate to booking form else login/signup form
+    if (userName) {
+      navigate('/book-session');
+    } else {
+      toast('Please Login first!')
+      navigate('/login')
+    }
+  }
+
   return (
-    <a href="#book-session" className='book-session-btn'>Book Session</a>
+    <button type="button" onClick={handleClick} className='book-session-btn'>Book Session</button>
   )
 }
