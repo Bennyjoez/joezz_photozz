453c8711533b405b7dbe56275600ebbfe85d0e1d
Benson Njuguna <njugunab655@gmail.com>
2024-03-05 17:34:12 +0300

    Add global error handler

---
+++ src/utils/handleErrors.ts
@@ -0,0 +1,32 @@
+import { toast } from "react-toastify";
+
+const handleErrors = (error: unknown) => {
+  const {data, statusText} = error.response
+  console.log(statusText)
+    // bad route
+    if (data.message && data.message.includes('Cannot find')) {
+      toast.error(data.message);
+    } else if (data.errors && data.errors.message.includes('date')) {
+      toast.error(data.errors.message);
+      const target = document.getElementById('date');
+      if(target) {
+        target.style.backgroundColor = '#ff9d9d';
+        setTimeout(() => {
+          target.style.backgroundColor = 'inherit';
+        }, 6000)
+      }
+    } else if (data.status == 'Unauthorized') {
+      // need to login
+      toast(`${data.status}: ${data.error.message}. Please login!`)
+    } else if(data.errors && data.errors.code === 11000) {
+      // duplicate key error
+      toast.error(data.errors.message);
+    } else if (statusText === 'Unauthorized') {
+      // wrong details
+      if (data.error && data.error.message) {
+        toast.error(data.error.message)
+      }
+    }
+};
+
+export default handleErrors;
