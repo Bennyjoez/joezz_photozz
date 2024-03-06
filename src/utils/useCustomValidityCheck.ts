b82987b1bfebea9db975e24ef71ddba333035d41
Benson Njuguna <njugunab655@gmail.com>
2024-03-06 19:38:53 +0300

    Clear bookings on logout or token expiry

--- src/utils/useCustomValidityCheck.ts
+++ src/utils/useCustomValidityCheck.ts
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useAppDispatch } from '../app/hooks';
 import { clearUser } from '../Features/user/userSlice';
 import isTokenExpired from './checkToken';
+import { clearBookings } from '../Features/bookings/bookingSlice';
 
 const useTokenValidityCheck = () => {
   const userToken = localStorage.getItem('authToken') || '';
@@ -12,6 +13,7 @@ const useTokenValidityCheck = () => {
     if (tokenExpired) {
       // Clear user details from the state if token is expired
       dispatch(clearUser());
+      dispatch(clearBookings());
     }
   }, [userToken, dispatch]);
 
