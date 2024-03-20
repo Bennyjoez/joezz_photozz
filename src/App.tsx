d993435ab3970aec64c5f9ac6879831c61f9e8e6
Benson Njuguna <njugunab655@gmail.com>
2024-03-20 13:07:29 +0300

    Provide react query to app

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,19 @@ import Layout from './Layout';
 import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import Navbar from './components/Navbar';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const App: React.FC = () => {
+  const queryClient = new QueryClient();
+
   return (
-    <Layout>
-      <Navbar />
-      <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
-      <Outlet />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Navbar />
+        <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
+        <Outlet />
+      </Layout>
+    </QueryClientProvider>
   );
 };
 
