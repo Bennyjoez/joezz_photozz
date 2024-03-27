3ac93f12fdca1a2a9db569a89c935508f3d2ef6e
Benson Njuguna <njugunab655@gmail.com>
2024-03-27 12:30:46 +0300

    Extract a review into a separate component

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -2,6 +2,11 @@ import { Review } from './reviews/review';
 import profile from "/profile.png"
 
 export default function Reviews() {
+
+  const handleClick = () => {
+    console.log("clicked")
+  }
+
   return (
     <section id='reviews-container'>
       <div className="reviews">
@@ -9,7 +14,7 @@ export default function Reviews() {
         <Review   profile={profile}  />
         <Review   profile={profile}  />
       </div>
-      <button className="book-session-btn ">Add Review</button>
+      <button className="book-session-btn" onClick={handleClick}>Add Review</button>
     </section>
   )
 }
--- src/components/reviews/review.tsx
+++ src/components/reviews/review.tsx
@@ -5,12 +5,14 @@ interface reviewProps {
 export function Review({
   profile
 }: reviewProps) {
-  return <div className='review'>
-          <img src={profile} className="review-profile" alt="reviewer" />
-          <div>
-            <h3>Benson Njuguna</h3>
-            <p>"These guys saved the day by covering my event and making it memorable to every detail"</p>
-          </div>
-        </div>;
+  return (
+    <div className='review'>
+      <img src={profile} className="review-profile" alt="reviewer" />
+      <div>
+        <h3>Benson Njuguna</h3>
+        <p>"These guys saved the day by covering my event and making it memorable to every detail"</p>
+      </div>
+    </div>
+  )
 }
   
