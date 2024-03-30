9a018520567f08d91759556683189e6cedad8994
Benson Njuguna <njugunab655@gmail.com>
2024-03-30 19:25:29 +0300

    Display reviews from the backend

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,23 +1,50 @@
 import { showReviewsPopup } from '../Features/modal/modalSlice';
 import { useAppDispatch } from '../app/hooks';
-import { Review } from './reviews/review';
-import profile from "/profile.png"
+import { Review, reviewDataProps } from './reviews/review';
+import { getReviews } from '../utils/reviewsEndpoints';
+import { useQuery } from '@tanstack/react-query';
+import Loading from './loading/Loading';
+import { useEffect } from 'react';
 
 export default function Reviews() {
-
   const dispatch = useAppDispatch();
+  const { isPending, isError, data, error, refetch } = useQuery({
+    queryKey: ["reviews"],
+    queryFn: () => getReviews(),
+    enabled: false,
+  });
+
+  useEffect(() => {
+    refetch();
+  }, [refetch])
 
   const handleClick = () => {
     dispatch(showReviewsPopup());
   }
 
+  if(isPending) {
+    return <Loading />
+  }
+
+  if (isError) {
+    return <div>{error.message}</div>;
+  }
+
+  const reviews = data?.data.data;
+
   return (
     <section id='reviews-container'>
-      <div className="reviews">
-        <Review   profile={profile}  />
-        <Review   profile={profile}  />
-        <Review   profile={profile}  />
+      {
+        !reviews
+        ?
+        <div>No reviews yet!</div>
+        :
+        <div className="reviews">
+        {reviews.map((review: reviewDataProps) => (
+          <Review key={review._id} reviewData={review} />
+        ))}
       </div>
+      }
       <button className="book-session-btn" onClick={handleClick}>Add Review</button>
     </section>
   )
