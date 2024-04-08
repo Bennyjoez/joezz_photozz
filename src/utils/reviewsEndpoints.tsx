607828df0fba4c6412225ad8bab82afff6caa7b0
Benson Njuguna <njugunab655@gmail.com>
2024-04-08 11:36:02 +0300

    Add review

--- src/utils/reviewsEndpoints.tsx
+++ src/utils/reviewsEndpoints.tsx
@@ -1,5 +1,10 @@
 import axiosInstance from "./axiosInstance";
 
+export interface detailsProps {
+  rating: string,
+  message: string,
+}
+
 export const getReviews = async () => {
   try {
     const endPoint = `/reviews`;
@@ -7,5 +12,14 @@ export const getReviews = async () => {
     return reviews;
   } catch (err) {
     console.log(err)
+  }
+};
+
+export const addReview = async (details: detailsProps) => {
+  try {
+    const endPoint = `/reviews`;
+    await axiosInstance.post(endPoint, details);
+  } catch (err) {
+    console.log(err);
   }
 };
