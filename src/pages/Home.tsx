8d61678de3b92d5f6c7ce9cff53ef09b4e9b869c
Benson Njuguna <njugunab655@gmail.com>
2024-04-10 18:32:57 +0300

    Lazy load contact form

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import ContactForm from "../components/ContactForm";
 import Hero from "../components/Hero";
 import Reviews from "../components/Reviews";
 import Services from "../components/services/Services";
@@ -7,6 +6,8 @@ import BookingForm from "../components/bookings/BookingPopup";
 import { useAppSelector } from "../app/hooks";
 import Gallery from "../components/gallery/Gallery";
 import AddReviewPopup from "../components/reviews/AddReviewPopup";
+import { Suspense, lazy } from "react";
+const ContactForm = lazy(() => import("../components/ContactForm"))
 
 export default function Home() {
   const showBookingsModal = useAppSelector((state) => state.modal.showBookingsModal);
@@ -23,7 +24,9 @@ export default function Home() {
         <h2 className="heading">Reviews</h2>
         <Reviews />
         <h2 className="heading">Contact Us</h2>
-        <ContactForm />
+        <Suspense fallback={<div>Loading...</div>}>
+          <ContactForm />
+        </Suspense>
       </div>
       {showBookingsModal && <BookingForm />}
       {showReviewsModal && <AddReviewPopup />}
