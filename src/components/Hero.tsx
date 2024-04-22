2120aaaa1a184076d53789de5eec77df0f9d7e3e
Benson Njuguna <njugunab655@gmail.com>
2024-04-22 17:51:11 +0300

    Hide arrow when the hero description is not in view

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,17 @@
+import { useState } from 'react';
 import { Description } from './hero/Description';
 import { MdKeyboardDoubleArrowDown } from "react-icons/md";
 
 export default function Hero() {
+  const [arrowIsVisible, setArrowIsVisible] = useState(true);
+
   return (
-    <div className="hero">
+    <div className="hero" >
       <div className="tile tilt-1"></div>
       <div className="tile tilt-2"></div>
       <div className="tile tilt-3"></div>
-      <Description     />
-      <MdKeyboardDoubleArrowDown className='arrow-down' />
+      <Description setArrowIsVisible={setArrowIsVisible}/>
+      {arrowIsVisible && <MdKeyboardDoubleArrowDown className='arrow-down' />}
     </div>
   );
 }
--- src/components/hero/Description.tsx
+++ src/components/hero/Description.tsx
@@ -1,9 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import BookBtn from "../bookings/BookBtn";
 
-export function Description() {
+interface descriptionProps {
+  setArrowIsVisible: (value: boolean) => void;
+}
+
+export function Description({setArrowIsVisible}: descriptionProps) {
   const [wordIndex, setWordIndex] = useState(0);
   const words = ["Anywhere,", "Everywhere,", "We are there"];
+  const componentRef = useRef(null);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -11,10 +16,31 @@ export function Description() {
     }, 3000);
 
     return () => clearInterval(intervalId);
-  }, []);
+  }, [words.length]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      const entry = entries[0];
+      if (entry.isIntersecting) {
+        setArrowIsVisible(true);
+      } else {
+        setArrowIsVisible(false);
+      }
+    });
+  
+    if (componentRef.current) {
+      observer.observe(componentRef.current);
+    }
+  
+    return () => {
+      if (componentRef.current) {
+        observer.unobserve(componentRef.current);
+      }
+    };
+  }, [componentRef, setArrowIsVisible]);
 
   return (
-    <div className="description animated-text">
+    <div className="description animated-text" ref={componentRef} >
       <p>Mark all your events</p>
       <h3>{words[wordIndex]}</h3>
       <BookBtn />
