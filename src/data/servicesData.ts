bc2cf06aec1d6df99e1069e343851bde4d5d4033
Benson Njuguna <njugunab655@gmail.com>
2024-04-29 19:34:37 +0300

    Set aside service data

---
+++ src/data/servicesData.ts
@@ -0,0 +1,23 @@
+import videoGraphyImg from "/joez_photozz_web/23.jpg";
+import eventPhotography from "/joez_photozz_web/24.jpg";
+import potraitPhotography from "/joez_photozz_web/25.jpg";
+
+const servicesOffered = [
+  {
+    title: "Potrait Photography",
+    description: "Get amazing portraits in matter of minutes. Book 2 days prior.",
+    img: potraitPhotography
+  },
+  {
+    title: "Event Photography",
+    description: "Capture your events in the most amazing way. Book before a week.",
+    img: eventPhotography
+  },
+  {
+    title: "Videography",
+    description: "Your life and events as cinema. Top notch filmmaking for your budget.",
+    img: videoGraphyImg
+  }
+];
+
+export default servicesOffered;
