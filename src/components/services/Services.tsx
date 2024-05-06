3beaa4d4d463b4976f2873ff8491daaac36b7a6a
Benson Njuguna <njugunab655@gmail.com>
2024-05-06 19:24:12 +0300

    Return different components for different routes

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -1,13 +1,46 @@
 import Service from "./Service";
 import servicesOffered from "../../data/servicesData";
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Services() {
+  const [onServicePage, setOnServicePage] = useState(false);
+  const location = useLocation().pathname;
+
+  useEffect(() => {
+    if (location === "/services") {
+      setOnServicePage(true);
+    }
+  }, [location]);
+
+  if (onServicePage) {
+    return (
+      <div className="services-grid">
+        {servicesOffered.map(({ title, description, img }, i) => {
+          return (
+            <Service
+              title={title}
+              description={description}
+              img={img}
+              count={i + 1}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+
   return (
     <div className="services">
       {servicesOffered.map(({ title, description, img }, i) => {
         return (
-          <Service title={title} description={description} img={img} count={i+1} />
-        )
+          <Service
+            title={title}
+            description={description}
+            img={img}
+            count={i + 1}
+          />
+        );
       })}
     </div>
   );
